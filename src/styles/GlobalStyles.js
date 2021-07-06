7a445b3918096ccddbb901d8cb2c0fde6f44b0cb
Marco-oli <marcoaurelio.oliveira@sambatech.com.br>
2021-07-06 06:40:36 -0300

    integrando eslint e style global

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+import colors from '../assets/colors'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        background-color: ${colors.darkSnow};
+        font-family: 'PT Sans';
+    }
+`;
